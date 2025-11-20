976c6874b85a4c8521a54a02ba45585002bf713d
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-20 14:40:23 -0300

    fix

--- main.cpp
+++ main.cpp
@@ -170,8 +170,10 @@ int main(int argc, char *argv[]) {
             .x = (float)x,
             .y = (float)y,
         };
-        vec2 p=(FC*2.-r)/r.y,l,v=p*(1.-(l+=abs(.7-dot(p,p))))/.2;for(float i;i++<8.;o+=(sin(v.xyyx)+1.)*abs(v.x-v.y)*.2)v+=cos(v.yx*i+vec2(0,i)+t)/i+.7;o=tanh(exp(p.y*vec4(1,-1,-2,0))*exp(-4.*l.x)/o);
-        // oss << (uint8_t)(o.x * MAX_VALUE) << (uint8_t)(o.y * MAX_VALUE) << (uint8_t)(o.z * MAX_VALUE);
+        vec2 p = (FC * 2. - r) / r.y, l, v = p * (1. - (l += abs(.7 - dot(p, p)))) / .2; for (float i; i++ < 8.; o += (sin(v.xyyx()) + 1.) * abs(v.x - v.y) * .2) v += cos(v.yx() * i + vec2(0, i) + t) / i + .7; o = tanh(exp(p.y * vec4(1, -1, -2, 0)) * exp(-4. * l.x) / o);
+
+        oss << (uint8_t)(o.x * MAX_VALUE) << (uint8_t)(o.y * MAX_VALUE)
+            << (uint8_t)(o.z * MAX_VALUE);
       }
     }
     output << oss.str();
