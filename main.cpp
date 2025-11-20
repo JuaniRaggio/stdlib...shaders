2e66f6a36f22e6d736b68ae774e97ff88ba6b3b6
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-20 14:34:34 -0300

    fix

--- main.cpp
+++ main.cpp
@@ -141,9 +141,9 @@ vec4 operator/(const vec4 &a, const vec4 &b) {
 
 int main(int argc, char *argv[]) {
 
-  for (uint8_t i{0}; i < FACTOR; ++i) {
+  for (uint8_t i = 0; i < FACTOR; ++i) {
 
-    std::string file_path = std::format("frames/out{}.ppm", i);
+    std::string file_path = std::format("frames/out{:02d}.ppm", i);
 
     std::ofstream output(file_path, std::ios::out);
 
@@ -163,21 +163,15 @@ int main(int argc, char *argv[]) {
         << std::format("{} {}\n", width, height)
         << std::format("{}\n", MAX_VALUE);
     float t = (float)i / 60;
-
-    for (uint64_t y{0}; y < height; ++y) {
-      for (uint64_t x{0}; x < width; ++x) {
-        vec4 o;
+    for (uint64_t y = 0; y < height; ++y) {
+      for (uint64_t x = 0; x < width; ++x) {
+        vec4 o = {0, 0, 0, 0};
         vec2 FC = {
             .x = (float)x,
             .y = (float)y,
         };
-        vec2 p = (FC * 2. - r) / r.y, l,
-             v = p * (1. - (l += abs(.7 - dot(p, p)))) / .2;
-        for (float i; i++ < 8.; o += (sin(v.xyyx()) + 1.) * abs(v.x - v.y) * .2)
-          v += cos(v.yx() * i + vec2(0, i) + t) / i + .7;
-        o = tanh(exp(p.y * vec4(1, -1, -2, 0)) * exp(-4. * l.x) / o);
-        oss << (uint8_t)(o.x * MAX_VALUE) << (uint8_t)(o.y * MAX_VALUE)
-            << (uint8_t)(o.z * MAX_VALUE);
+        vec2 p=(FC*2.-r)/r.y,l,v=p*(1.-(l+=abs(.7-dot(p,p))))/.2;for(float i;i++<8.;o+=(sin(v.xyyx)+1.)*abs(v.x-v.y)*.2)v+=cos(v.yx*i+vec2(0,i)+t)/i+.7;o=tanh(exp(p.y*vec4(1,-1,-2,0))*exp(-4.*l.x)/o);
+        // oss << (uint8_t)(o.x * MAX_VALUE) << (uint8_t)(o.y * MAX_VALUE) << (uint8_t)(o.z * MAX_VALUE);
       }
     }
     output << oss.str();
