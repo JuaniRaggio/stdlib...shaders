294815925de8e3dc72ae6fb63e0d83aa810c58d3
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-11-20 00:44:49 -0300

    generating 60 ppms

--- main.cpp
+++ main.cpp
@@ -12,30 +12,38 @@
 
 int main(int argc, char *argv[]) {
 
-  std::string const file_name("out.ppm");
+  for (int i{0}; i < FACTOR; ++i) {
 
-  std::ofstream output(file_name, std::ios::out);
+    std::string file_path = std::format("frames/out{}.ppm", i);
 
-  if (!output.is_open()) {
-    std::cerr << std::format("Unable to open file {}", file_name);
-    return UNABLE_TO_OPEN_FILE;
-  }
+    std::ofstream output(file_path, std::ios::out);
 
-  std::uint16_t width{16 * FACTOR};
-  std::uint16_t height{9 * FACTOR};
-  std::ostringstream oss;
-  oss << "P6\n"
-      << std::format("%d %d\n", width, height)
-      << std::format("%d\n", MAX_VALUE);
-
-  for (uint64_t y {0}; y < height; ++y) {
-    for (uint64_t x {0}; x < width; ++x) {
-      oss << 0xFF << 0x00 << 0x00;
+    if (!output.is_open()) {
+      std::cerr << std::format("Unable to open file {}", file_path);
+      return UNABLE_TO_OPEN_FILE;
     }
+
+    std::uint16_t width{16 * FACTOR};
+    std::uint16_t height{9 * FACTOR};
+    std::ostringstream oss;
+    oss << "P6\n"
+        << std::format("%d %d\n", width, height)
+        << std::format("%d\n", MAX_VALUE);
+
+    for (uint64_t y{0}; y < height; ++y) {
+      for (uint64_t x{0}; x < width; ++x) {
+        if (((x + i) / 60 + (y + i) / 60) % 2) {
+          // This is like R G B
+          oss << 0xFF << 0x00 << 0x00;
+        } else {
+          oss << 0x00 << 0x00 << 0x00;
+        }
+      }
+    }
+    output << oss.str();
+    output.close();
+    std::cout << std::format("Generated {}\n", file_path);
   }
-  output << oss.str();
-  output.close();
-  std::cout << std::format("Generated {}\n", file_name);
 
   return 0;
 }
